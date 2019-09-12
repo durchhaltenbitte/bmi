696226751c9f6be237397dc5ff3c3da649ceb4f9
durchhaltenbitte <shuang.ind@gmail.com>
2019-09-12 10:45:45 +0200

    final

--- bmi.py
+++ bmi.py
@@ -5,14 +5,14 @@ weight = float(weight)
 bmi = weight/height**2
 
 if bmi < 18.5:
-	print('您的BMI为:', bmi, '属于体重过轻')
+	print('您的BMI为:', round(bmi,1), '属于体重过轻')
 elif bmi >= 18.5 and bmi < 24:
-	print('您的BMI为:', bmi, '属于正常范围')
+	print('您的BMI为:', round(bmi,1), '属于正常范围')
 elif bmi >= 24 and bmi < 27:
-	print('您的BMI为:', bmi, '属于过重')
+	print('您的BMI为:', round(bmi,1), '属于过重')
 elif bmi >= 27 and bmi < 30:
-	print('您的BMI为:', bmi, '属于轻度肥胖')
+	print('您的BMI为:', round(bmi,1), '属于轻度肥胖')
 elif bmi >= 30 and bmi < 35:
-	print('您的BMI为:', bmi, '属于中度肥胖')
+	print('您的BMI为:', round(bmi,1), '属于中度肥胖')
 else: 
-	print('您的BMI为:', bmi, '属于重度肥胖')
+	print('您的BMI为:', round(bmi,1), '属于重度肥胖')
